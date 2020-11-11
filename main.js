cb3de6adf98a6477ee6f6974b95e9a6324a7249f
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-11-11 18:03:29 +0200

    3

--- main.js
+++ main.js
@@ -7,3 +7,37 @@ console.log("Labas rytas")
 console.log('daniel gurevich')
 console.log(3, 15)
 
+/*kintamieji*/
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 3;
+
+console.log ("pirmasis skacius turetu buti 2.")
+console.log (pirmasSkaicius)
+
+console.log ("antras skacius turetu buti 3.")
+console.log (antrasSkaicius)
+
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log(suma);
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=');
+const vardas ='vardenis'
+
+console.log(vardas)
+
+
+const suo = 'rikis'
+const maistas = 'kaulas'
+const sakinys = suo + ' megstamas maistas yra ' + maistas + '.';
+console.log(sakinys)
+
+const heroName ='Chuck';
+const heroLastname = 'Norris';
+const birth = 1940;
+
+const sakinys3 = heroName+' ' + heroLastname +' '+ (2020 - birth)+' ' + 'years old.';
+
+console.log(sakinys3);
+
+console.clear()
