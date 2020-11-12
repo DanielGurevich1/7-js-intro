ed992c67ae956d196c655c76971753437f1572d1
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-11-12 11:36:27 +0200

    if

---
+++ if.js
@@ -0,0 +1,10 @@
+const accountBalance = 201;
+const transfer = 200;
+
+if (accountBalance < transfer) {
+    console.log('banko zinute: saskaitoje truksta pinigu.');
+}
+if (accountBalance >= transfer) {
+    console.log('banko zinute: Pavedimas padarytas.');
+}
+
