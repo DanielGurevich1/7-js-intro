56eb3f678feb01c5bb33d8c3e19e06c0715a08f6
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-11-12 12:29:54 +0200

    else if

--- if.js
+++ if.js
@@ -1,4 +1,4 @@
-const accountBalance = 201;
+const accountBalance = 200;
 const transfer = 200;
 
 if (accountBalance < transfer) {
@@ -6,5 +6,28 @@ if (accountBalance < transfer) {
 }
 if (accountBalance >= transfer) {
     console.log('banko zinute: Pavedimas padarytas.');
+} else {console.log('banko zinute: saskaitoje truksta pinigu.');
+
 }
 
+/* if, 
+<, >, >=, <=, ==, !=, ===, !==
+naudotini: <, >, >=, <=, !=, ===, !==
+nenaudotini: ==,  !==
+draudziami: <!, !<, =>, =<
+*/
+
+/*
+if () {}
+if () {} else {} 
+if () {} else if {} ... {} else if {}
+ if () {} else if {} ... {} else if {} else {}
+*/
+
+const akys = 'zalios';
+
+if (akys === 'melynos') {
+    console.log('grazios sirdies zmogus');
+    } else { 
+        console.log('ne melynakis' );}
+         
