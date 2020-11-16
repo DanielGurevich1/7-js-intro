e281b1b526643ec3b26b0257e3949a5150383afc
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-11-16 17:57:34 +0200

    more or less

--- empty.js
+++ empty.js
@@ -3,3 +3,20 @@ function tusciaFunkcija() {
 }
 
 console.log(tusciaFunkcija());
+
+function multiply(a, b) {
+    if (typeof a !== 'number') {
+console.log('error: pirmoji reiksme yra ne ..')
+    }
+
+    if (typeof b !== 'number') {
+        console.log('error: antroji reiksme yra ne ..')
+    }
+    return a * b;
+}
+
+console.log(multiply(2, 2));
+console.log(multiply(2, 5));
+console.log(multiply(-2, 11));
+console.log(multiply(2, 2));
+console.log(multiply(2, blur));
